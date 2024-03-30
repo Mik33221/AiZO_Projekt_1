1a1555c50d74232418376d32632d9b79d6f01686
Mik33221 <miki103gekon@gmail.com>
2024-03-30 15:59:44 +0100

    Basic Structure

--- Menu.cpp
+++ Menu.cpp
@@ -1,1 +1,9 @@
 #include "Menu.h"
+
+#include "iostream"
+
+using namespace std;
+
+void Menu::run() {
+	cout << "Hello Word!";
+}
--- Menu.h
+++ Menu.h
@@ -1,5 +1,6 @@
-#pragma once
+
 class Menu
 {
+public:
+	void run();
 };
-
