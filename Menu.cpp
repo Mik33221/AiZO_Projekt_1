0ce76f2f08ce6c9f3f2ccbee92c552b0dc404b39
Mik33221 <miki103gekon@gmail.com>
2024-04-02 13:31:42 +0200

    menu optimalization

--- Menu.cpp
+++ Menu.cpp
@@ -10,17 +10,21 @@
 using namespace std;
 
 //work functions
-void viewTab(TYPE* tab) {
+template<typename T>
+void viewTab(T* tab) {
 	for (int i = 0; i < N; i++) {
 		std::cout << tab[i] << std::endl;
 	}
 
 	std::cout << std::endl;
 }
-void isSorted(TYPE* tab) {
-	for (int i = 0; i < N; i++) {
-		;
+
+template<typename T>
+string isSorted(T* tab) {
+	for (int i = 1; i < N; i++) {
+		if (tab[i] < tab[i - 1]) return "Table not sorted\n\n";
 	}
+	return "Table sorted\n\n";
 }
 
 //menu
@@ -32,16 +36,16 @@ void Menu::run()
 void Menu::workflow()
 {
 	TableManager<TYPE> table(N);
-	
 
-	viewTab(table.tab);
+	//viewTab<>(table.tab);
 
-	viewTab(table.tabCopy);
+	//viewTab(table.tabCopy);
 	
 	InsertSort sort1;	//drop polymorphism in this case
 	auto time = sort1.sort<TYPE>(table.tabCopy, N);
 	
-	viewTab(table.tabCopy);
+	//viewTab(table.tabCopy);
+	std::cout << isSorted(table.tabCopy);
 
 	std::cout << "Time: " << time << "ms" << std::endl;
 
